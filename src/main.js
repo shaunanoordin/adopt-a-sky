5c7cbed527dd9f73e67139c0fa4049e36e529610
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-11-06 19:43:34 +0000

    auth: clean up code. Present auth status on frontend

--- src/main.js
+++ src/main.js
@@ -1,71 +1,118 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import decodeJWT from './util/decodeJWT.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+function $ (arg) {
+  return document.querySelector(arg)
+}
 
 class WebApp {
   constructor () {
-    console.log('+++ WebApp.constructor()')
+    this.userToken = undefined
+
+    this.doSignOut = this.doSignOut.bind(this)
+
+    $('#signout-button').addEventListener('click', this.doSignOut)
   }
 
+  // Starts the app, once the window has fully loaded.
+  // - Triggered by window.onload.
   start () {
-    console.log('+++ WebApp.start()')
+    console.log('start()')
+    this.update()
     this.checkAuth()
   }
 
   // Check if user is currently authenticated, by confirming with the server.
+  // - Triggered when the app starts (to check if the user "has previously
+  //   signed in", aka has an existing JWT saved in localStorage) and when
+  //   the "sign in" action successfully completes.
   async checkAuth () {
-    const userToken = localStorage?.getItem('userToken')  || ''
+    console.log('checkAuth()')
 
-    const res = await fetch('/api/auth', {
-      method: 'POST',
-      headers: {
-        'Authorization': userToken ? `Bearer ${userToken}` : undefined,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        foo: 'bar'
-      }),
-    })
+    try {
+      const userToken = localStorage?.getItem('userToken')  || ''
+      this.userToken = userToken
 
-    const resBody = await res.json()
-    console.log('+++ resBody: ', resBody)
+      // Hey server, can you please confirm that user X with token Y is who
+      // they say they are?
+      const res = await fetch('/api/auth', {
+        method: 'POST',
+        headers: {
+          'Authorization': userToken ? `Bearer ${userToken}` : undefined,
+          'Content-Type': 'application/json',
+        },
+      })
+
+      // If we don't get a 200 response, user isn't authenticated properly.
+      if (res.status !== 200) throw new Error(`/api/auth returned ${res.status}`)
+
+      // If we get a 200 response, we can confirm that user is legit.
+      this.update()
+
+    } catch (err) {
+
+      console.error(err)
+      this.doSignOut()
+
+    }
   }
 
+  // Handles successful sign in event.
+  // - `token` is a JSON Web Token (jwt) provided by Google, from the
+  //   "sign in" event, which contains
+  // - Triggered by Sign In With Google's callback.
+  // - This token will be saved to local storage
   onSignIn (token) {
-    const userInfo = decodeJWT(token)
-    console.log('+++ userInfo: ', userInfo)
-
-    localStorage?.setItem('userToken', token)
+    // Sanity check: if there's no token, sign out.
+    if (!token) return this.doSignOut()
     
-    this.checkAuth()
+    // Debug
+    const userInfo = decodeJWT(token)
+    console.log('onSignIn(), userInfo: ', userInfo)
 
-    // TODO
+    // Save user token (JWT)
+    localStorage?.setItem('userToken', token)
+    this.userToken = token
+    
+    // Ask server to confirm that user is legit.
+    this.checkAuth()
   }
 
+  // Signs out the current user.
   doSignOut () {
-    // TODO
+    console.log('doSignOut()')
+    localStorage?.removeItem('userToken')
+    this.userToken = undefined
+
+    this.update()
+  }
+
+  // Updates the webpage to match the app status.
+  update () {
+    console.log('update()')
+
+    // Update header
+    const userToken = this.userToken
+    const userInfo = decodeJWT(userToken)
+    console.log('userInfo: ', userInfo)
+
+    if (userToken) {
+
+      $('#signout-button').style.display = 'block'
+      $('.g_id_signin').style.display = 'none'
+
+      $('#user-details').style.display = 'block'
+      $('#user-details').innerHTML = `Signed in as ${userInfo?.name || '???'}`
+
+    } else {
+
+      $('#signout-button').style.display = 'none'
+      $('.g_id_signin').style.display = 'block'
+
+      $('#user-details').style.display = 'none'
+      $('#user-details').innerHTML = ''
+
+    }
   }
 }
 
--- src/util/decodeJWT.js
+++ src/util/decodeJWT.js
@@ -3,15 +3,23 @@ Decodes a JSON Web Token.
  */
 
 export default function decodeJWT (token) {
-  let base64Url = token.split('.')[1]
-  let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-  let jsonPayload = decodeURIComponent(
-    atob(base64)
-      .split('')
-      .map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-      })
-      .join('')
-  )
-  return JSON.parse(jsonPayload)
+  try {
+
+    let base64Url = token.split('.')[1]
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    let jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+        })
+        .join('')
+    )
+    return JSON.parse(jsonPayload)
+
+  } catch (err) {
+
+    return undefined
+    
+  }
 }
