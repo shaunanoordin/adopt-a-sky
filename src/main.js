bfec1a271628da359008d035511c9f9fefc85e20
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-11-06 20:37:15 +0000

    auth: add .userChecked. Improve docs.

--- src/main.js
+++ src/main.js
@@ -7,15 +7,29 @@ function $ (arg) {
 
 class WebApp {
   constructor () {
+    // JSON Web Token.
+    // - Contains user details.
+    // - This is the "stamp of approval" received from Google upon successfully
+    //   signing in.
+    // - We also store this in localStorage.
     this.userToken = undefined
 
-    this.doSignOut = this.doSignOut.bind(this)
+    // Determines if we know if the user is legit.
+    // - True if EITHER checkAuth() has confirmed user is legit OR user is
+    //   signed out.
+    // - False when user has just loaded the app, OR the user has just started
+    //   the sign in process.
+    // - Used to see when it's safe to serve user-related features.
+    this.userChecked = false
 
+    // Bind functions and event handlers.
+    this.doSignOut = this.doSignOut.bind(this)
     $('#signout-button').addEventListener('click', this.doSignOut)
   }
 
   // Starts the app, once the window has fully loaded.
   // - Triggered by window.onload.
+  // - Initiates the "do I have a legit user?" check.
   start () {
     console.log('start()')
     this.update()
@@ -26,12 +40,16 @@ class WebApp {
   // - Triggered when the app starts (to check if the user "has previously
   //   signed in", aka has an existing JWT saved in localStorage) and when
   //   the "sign in" action successfully completes.
+  // - One way or another, will set userChecked = true once checks are done.
   async checkAuth () {
     console.log('checkAuth()')
 
     try {
-      const userToken = localStorage?.getItem('userToken')  || ''
+      const userToken = localStorage?.getItem('userToken')
       this.userToken = userToken
+
+      // If there's no user token, let's not bother asking.
+      if (!userToken) return this.doSignOut()
 
       // Hey server, can you please confirm that user X with token Y is who
       // they say they are?
@@ -42,6 +60,9 @@ class WebApp {
           'Content-Type': 'application/json',
         },
       })
+
+      // Indicate that we've successfully checked if the user is legit. 
+      this.userChecked = true
 
       // If we don't get a 200 response, user isn't authenticated properly.
       if (res.status !== 200) throw new Error(`/api/auth returned ${res.status}`)
@@ -65,16 +86,13 @@ class WebApp {
   onSignIn (token) {
     // Sanity check: if there's no token, sign out.
     if (!token) return this.doSignOut()
-    
-    // Debug
-    const userInfo = decodeJWT(token)
-    console.log('onSignIn(), userInfo: ', userInfo)
 
     // Save user token (JWT)
     localStorage?.setItem('userToken', token)
     this.userToken = token
     
     // Ask server to confirm that user is legit.
+    this.userChecked = false
     this.checkAuth()
   }
 
@@ -83,6 +101,7 @@ class WebApp {
     console.log('doSignOut()')
     localStorage?.removeItem('userToken')
     this.userToken = undefined
+    this.userChecked = true  // We've signed out the user, so we know for sure we DON'T have a user.
 
     this.update()
   }
