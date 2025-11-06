eac73a78707e8345c751730e1eeb512f3a76111a
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2025-11-06 15:34:55 +0000

    auth: implement verifyGoogleIdentityToken()

--- server/api/auth.js
+++ server/api/auth.js
@@ -3,14 +3,31 @@ Authenticate User
  */
 
 import decodeJWT from '../../src/util/decodeJWT.js'
+import { OAuth2Client } from  'google-auth-library'
 
-export default async function api_test (clientRequest, serverResponse) {
+async function verifyGoogleIdentityToken (token) {
+  const oauthClient = new OAuth2Client()
+  const ticket = await oauthClient.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+  })
+  const payload = ticket.getPayload()
+  const userId = payload['sub']  // Unique user ID. Use as primary key; don't use email as that may change.
+
+  console.log('+++ verify START')
+  console.log(userId)
+  console.log('+++ verify END')
+}
+
+export default async function api_auth (clientRequest, serverResponse) {
   try {
     console.log('+++ START')
     const auth = clientRequest.get('Authorization')
     const userToken = auth?.match(/(^Bearer )(.*)/)?.[2]
     console.log(userToken)
     console.log('+++ END')
+
+    await verifyGoogleIdentityToken(userToken)
     
     serverResponse
     .status(200)
@@ -21,6 +38,8 @@ export default async function api_test (clientRequest, serverResponse) {
     })
 
   } catch (err) {
+
+    console.error(err)
 
     serverResponse
     .status(500)
